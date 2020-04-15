fabe6720e045d1cf2e8231fbff97073fa96a9bba
unknown <jpatek@ubiqsecurity.com>
2020-04-15 00:45:50 -0700

    started to outline client and server design

--- src/include/cchat.h
+++ src/include/cchat.h
@@ -1,21 +1,45 @@
 #include <iocolors.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+typedef SOCKET cchat_socket_t;
+
+#else
+
+typedef int32_t cchat_socket_t;
+
+
+#endif
 
 typedef struct
 {
-    int a;
+    cchat_socket_t snd_sock;
+    cchat_socket_t rcv_sock;
+    
 } 
 cchat_client_t;
 
+typedef struct
+{
+    cchat_socket_t srv_sock;
+    int a;
+} 
+cchat_server_t;
+
 int cchat_client_argparse(
     cchat_client_t * const client, 
-    const char ** const args, 
+    const char ** const argv, 
     const uint32_t argc);
 
 int cchat_client_init(
     cchat_client_t * const client);
-
-typedef struct
-{
-    int a;
-} 
-cchat_server_t;
